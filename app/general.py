e951f4896a2301ebe198fe86f9745ec2cbce753d
Ashraful Sheikh <ashrafulsf@gmail.com>
2015-04-29 14:28:06 -0700

    add new html

--- app/general.py
+++ app/general.py
@@ -8,9 +8,7 @@ from lib.mongo import db
 from lib import testmongo
 
 
-class FBHandler(app.basic.BaseHandler):
-	def my_callback(result, error):
-		print 'result', repr(result)
+class HomeHandler(app.basic.BaseHandler):
 	def get(self):
 		self.render('index.html')
 
@@ -50,3 +48,115 @@ class ScorpioHandler(app.basic.BaseHandler):
 class SagittariusHandler(app.basic.BaseHandler):
 	def get(self):
 		self.render('sagittarius.html')
+
+
+class CapricornExploreHandler(app.basic.BaseHandler):
+	def get(self):
+		self.render('overview-capricorn.html')
+class AquariusExploreHandler(app.basic.BaseHandler):
+	def get(self):
+		self.render('overview-aquarius.html')
+class PiscesExploreHandler(app.basic.BaseHandler):
+	def get(self):
+		self.render('overview-pisces.html')
+class AriesExploreHandler(app.basic.BaseHandler):
+	def get(self):
+		self.render('overview-aries.html')
+class TaurusExploreHandler(app.basic.BaseHandler):
+	def get(self):
+		self.render('overview-taurus.html')
+class GeminiExploreHandler(app.basic.BaseHandler):
+	def get(self):
+		self.render('overview-gemini.html')
+class CancerExploreHandler(app.basic.BaseHandler):
+	def get(self):
+		self.render('overview-cancer.html')
+class LeoExploreHandler(app.basic.BaseHandler):
+	def get(self):
+		self.render('overview-leo.html')
+class VirgoExploreHandler(app.basic.BaseHandler):
+	def get(self):
+		self.render('overview-virgo.html')
+class LibraExploreHandler(app.basic.BaseHandler):
+	def get(self):
+		self.render('overview-libra.html')
+class ScorpioExploreHandler(app.basic.BaseHandler):
+	def get(self):
+		self.render('overview-scorpio.html')
+class SagittariusExploreHandler(app.basic.BaseHandler):
+	def get(self):
+		self.render('overview-sagittarius.html')
+
+class CapricornCompatibilityHandler(app.basic.BaseHandler):
+	def get(self):
+		self.render('compatibility-capricorn.html')
+class AquariusCompatibilityHandler(app.basic.BaseHandler):
+	def get(self):
+		self.render('compatibility-aquarius.html')
+class PiscesCompatibilityHandler(app.basic.BaseHandler):
+	def get(self):
+		self.render('compatibility-pisces.html')
+class AriesCompatibilityHandler(app.basic.BaseHandler):
+	def get(self):
+		self.render('compatibility-aries.html')
+class TaurusCompatibilityHandler(app.basic.BaseHandler):
+	def get(self):
+		self.render('compatibility-taurus.html')
+class GeminiCompatibilityHandler(app.basic.BaseHandler):
+	def get(self):
+		self.render('compatibility-gemini.html')
+class CancerCompatibilityHandler(app.basic.BaseHandler):
+	def get(self):
+		self.render('compatibility-cancer.html')
+class LeoCompatibilityHandler(app.basic.BaseHandler):
+	def get(self):
+		self.render('compatibility-leo.html')
+class VirgoCompatibilityHandler(app.basic.BaseHandler):
+	def get(self):
+		self.render('compatibility-virgo.html')
+class LibraCompatibilityHandler(app.basic.BaseHandler):
+	def get(self):
+		self.render('compatibility-libra.html')
+class ScorpioCompatibilityHandler(app.basic.BaseHandler):
+	def get(self):
+		self.render('compatibility-scorpio.html')
+class SagittariusCompatibilityHandler(app.basic.BaseHandler):
+	def get(self):
+		self.render('compatibility-sagittarius.html')
+
+class CapricornFamousHandler(app.basic.BaseHandler):
+	def get(self):
+		self.render('famous-capricorn.html')
+class AquariusFamousHandler(app.basic.BaseHandler):
+	def get(self):
+		self.render('famous-aquarius.html')
+class PiscesFamousHandler(app.basic.BaseHandler):
+	def get(self):
+		self.render('famous-pisces.html')
+class AriesFamousHandler(app.basic.BaseHandler):
+	def get(self):
+		self.render('famous-aries.html')
+class TaurusFamousHandler(app.basic.BaseHandler):
+	def get(self):
+		self.render('famous-taurus.html')
+class GeminiFamousHandler(app.basic.BaseHandler):
+	def get(self):
+		self.render('famous-gemini.html')
+class CancerFamousHandler(app.basic.BaseHandler):
+	def get(self):
+		self.render('famous-cancer.html')
+class LeoFamousHandler(app.basic.BaseHandler):
+	def get(self):
+		self.render('famous-leo.html')
+class VirgoFamousHandler(app.basic.BaseHandler):
+	def get(self):
+		self.render('famous-virgo.html')
+class LibraFamousHandler(app.basic.BaseHandler):
+	def get(self):
+		self.render('famous-libra.html')
+class ScorpioFamousHandler(app.basic.BaseHandler):
+	def get(self):
+		self.render('famous-scorpio.html')
+class SagittariusFamousHandler(app.basic.BaseHandler):
+	def get(self):
+		self.render('famous-sagittarius.html')
--- tornado_server.py
+++ tornado_server.py
@@ -32,7 +32,7 @@ class Application(tornado.web.Application):
         the endpoints this torando instance provides
         """
         handlers = [
-            (r"/", app.general.FBHandler),
+            (r"/", app.general.HomeHandler),
             (r"/capricorn", app.general.CapricornHandler),
             (r"/aquarius", app.general.AquariusHandler),
             (r"/pisces", app.general.PiscesHandler),
@@ -45,6 +45,45 @@ class Application(tornado.web.Application):
             (r"/libra", app.general.LibraHandler),
             (r"/scorpio", app.general.ScorpioHandler),
             (r"/sagittarius", app.general.SagittariusHandler),
+
+            (r"/capricorn/explore", app.general.CapricornExploreHandler),
+            (r"/aquarius/explore", app.general.AquariusExploreHandler),
+            (r"/pisces/explore", app.general.PiscesExploreHandler),
+            (r"/aries/explore", app.general.AriesExploreHandler),
+            (r"/taurus/explore", app.general.TaurusExploreHandler),
+            (r"/gemini/explore", app.general.GeminiExploreHandler),
+            (r"/cancer/explore", app.general.CancerExploreHandler),
+            (r"/leo/explore", app.general.LeoExploreHandler),
+            (r"/virgo/explore", app.general.VirgoExploreHandler),
+            (r"/libra/explore", app.general.LibraExploreHandler),
+            (r"/scorpio/explore", app.general.ScorpioExploreHandler),
+            (r"/sagittarius/explore", app.general.SagittariusExploreHandler),
+
+            (r"/capricorn/compatibility", app.general.CapricornCompatibilityHandler),
+            (r"/aquarius/compatibility", app.general.AquariusCompatibilityHandler),
+            (r"/pisces/compatibility", app.general.PiscesCompatibilityHandler),
+            (r"/aries/compatibility", app.general.AriesCompatibilityHandler),
+            (r"/taurus/compatibility", app.general.TaurusCompatibilityHandler),
+            (r"/gemini/compatibility", app.general.GeminiCompatibilityHandler),
+            (r"/cancer/compatibility", app.general.CancerCompatibilityHandler),
+            (r"/leo/compatibility", app.general.LeoCompatibilityHandler),
+            (r"/virgo/compatibility", app.general.VirgoCompatibilityHandler),
+            (r"/libra/compatibility", app.general.LibraCompatibilityHandler),
+            (r"/scorpio/compatibility", app.general.ScorpioCompatibilityHandler),
+            (r"/sagittarius/compatibility", app.general.SagittariusCompatibilityHandler),
+
+            (r"/capricorn/famous", app.general.CapricornFamousHandler),
+            (r"/aquarius/famous", app.general.AquariusFamousHandler),
+            (r"/pisces/famous", app.general.PiscesFamousHandler),
+            (r"/aries/famous", app.general.AriesFamousHandler),
+            (r"/taurus/famous", app.general.TaurusFamousHandler),
+            (r"/gemini/famous", app.general.GeminiFamousHandler),
+            (r"/cancer/famous", app.general.CancerFamousHandler),
+            (r"/leo/famous", app.general.LeoFamousHandler),
+            (r"/virgo/famous", app.general.VirgoFamousHandler),
+            (r"/libra/famous", app.general.LibraFamousHandler),
+            (r"/scorpio/famous", app.general.ScorpioFamousHandler),
+            (r"/sagittarius/famous", app.general.SagittariusFamousHandler),
         ]
 
         tornado.web.Application.__init__(self, handlers, **app_settings)
