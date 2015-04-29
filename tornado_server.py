415fea06cda6c07c8985622109786eab9f8ab48b
Ashraful Sheikh <ashrafulsf@gmail.com>
2015-04-29 15:03:06 -0700

    make server.py heroku compatible

--- tornado_server.py
+++ tornado_server.py
@@ -13,88 +13,74 @@ import settings
 import app.basic
 import app.general
 
-from pyechonest import config
-config.ECHO_NEST_API_KEY="CGR1FESZMSLL0AITW"
 
-class Application(tornado.web.Application):
-    def __init__(self):
-        debug = (tornado.options.options.environment == "dev")
 
-        app_settings = {
-            "cookie_secret" : "change_me",
-            "login_url": "/",
-            "debug": debug,
-            "static_path" : os.path.join(os.path.dirname(__file__), "static"),
-            "template_path" : os.path.join(os.path.dirname(__file__), "templates"),
-        }
+handlers = handlers = [
+    (r"/", app.general.HomeHandler),
+    (r"/capricorn", app.general.CapricornHandler),
+    (r"/aquarius", app.general.AquariusHandler),
+    (r"/pisces", app.general.PiscesHandler),
+    (r"/aries", app.general.AriesHandler),
+    (r"/taurus", app.general.TaurusHandler),
+    (r"/gemini", app.general.GeminiHandler),
+    (r"/cancer", app.general.CancerHandler),
+    (r"/leo", app.general.LeoHandler),
+    (r"/virgo", app.general.VirgoHandler),
+    (r"/libra", app.general.LibraHandler),
+    (r"/scorpio", app.general.ScorpioHandler),
+    (r"/sagittarius", app.general.SagittariusHandler),
 
-        """
-        the endpoints this torando instance provides
-        """
-        handlers = [
-            (r"/", app.general.HomeHandler),
-            (r"/capricorn", app.general.CapricornHandler),
-            (r"/aquarius", app.general.AquariusHandler),
-            (r"/pisces", app.general.PiscesHandler),
-            (r"/aries", app.general.AriesHandler),
-            (r"/taurus", app.general.TaurusHandler),
-            (r"/gemini", app.general.GeminiHandler),
-            (r"/cancer", app.general.CancerHandler),
-            (r"/leo", app.general.LeoHandler),
-            (r"/virgo", app.general.VirgoHandler),
-            (r"/libra", app.general.LibraHandler),
-            (r"/scorpio", app.general.ScorpioHandler),
-            (r"/sagittarius", app.general.SagittariusHandler),
+    (r"/capricorn/explore", app.general.CapricornExploreHandler),
+    (r"/aquarius/explore", app.general.AquariusExploreHandler),
+    (r"/pisces/explore", app.general.PiscesExploreHandler),
+    (r"/aries/explore", app.general.AriesExploreHandler),
+    (r"/taurus/explore", app.general.TaurusExploreHandler),
+    (r"/gemini/explore", app.general.GeminiExploreHandler),
+    (r"/cancer/explore", app.general.CancerExploreHandler),
+    (r"/leo/explore", app.general.LeoExploreHandler),
+    (r"/virgo/explore", app.general.VirgoExploreHandler),
+    (r"/libra/explore", app.general.LibraExploreHandler),
+    (r"/scorpio/explore", app.general.ScorpioExploreHandler),
+    (r"/sagittarius/explore", app.general.SagittariusExploreHandler),
 
-            (r"/capricorn/explore", app.general.CapricornExploreHandler),
-            (r"/aquarius/explore", app.general.AquariusExploreHandler),
-            (r"/pisces/explore", app.general.PiscesExploreHandler),
-            (r"/aries/explore", app.general.AriesExploreHandler),
-            (r"/taurus/explore", app.general.TaurusExploreHandler),
-            (r"/gemini/explore", app.general.GeminiExploreHandler),
-            (r"/cancer/explore", app.general.CancerExploreHandler),
-            (r"/leo/explore", app.general.LeoExploreHandler),
-            (r"/virgo/explore", app.general.VirgoExploreHandler),
-            (r"/libra/explore", app.general.LibraExploreHandler),
-            (r"/scorpio/explore", app.general.ScorpioExploreHandler),
-            (r"/sagittarius/explore", app.general.SagittariusExploreHandler),
+    (r"/capricorn/compatibility", app.general.CapricornCompatibilityHandler),
+    (r"/aquarius/compatibility", app.general.AquariusCompatibilityHandler),
+    (r"/pisces/compatibility", app.general.PiscesCompatibilityHandler),
+    (r"/aries/compatibility", app.general.AriesCompatibilityHandler),
+    (r"/taurus/compatibility", app.general.TaurusCompatibilityHandler),
+    (r"/gemini/compatibility", app.general.GeminiCompatibilityHandler),
+    (r"/cancer/compatibility", app.general.CancerCompatibilityHandler),
+    (r"/leo/compatibility", app.general.LeoCompatibilityHandler),
+    (r"/virgo/compatibility", app.general.VirgoCompatibilityHandler),
+    (r"/libra/compatibility", app.general.LibraCompatibilityHandler),
+    (r"/scorpio/compatibility", app.general.ScorpioCompatibilityHandler),
+    (r"/sagittarius/compatibility", app.general.SagittariusCompatibilityHandler),
 
-            (r"/capricorn/compatibility", app.general.CapricornCompatibilityHandler),
-            (r"/aquarius/compatibility", app.general.AquariusCompatibilityHandler),
-            (r"/pisces/compatibility", app.general.PiscesCompatibilityHandler),
-            (r"/aries/compatibility", app.general.AriesCompatibilityHandler),
-            (r"/taurus/compatibility", app.general.TaurusCompatibilityHandler),
-            (r"/gemini/compatibility", app.general.GeminiCompatibilityHandler),
-            (r"/cancer/compatibility", app.general.CancerCompatibilityHandler),
-            (r"/leo/compatibility", app.general.LeoCompatibilityHandler),
-            (r"/virgo/compatibility", app.general.VirgoCompatibilityHandler),
-            (r"/libra/compatibility", app.general.LibraCompatibilityHandler),
-            (r"/scorpio/compatibility", app.general.ScorpioCompatibilityHandler),
-            (r"/sagittarius/compatibility", app.general.SagittariusCompatibilityHandler),
+    (r"/capricorn/famous", app.general.CapricornFamousHandler),
+    (r"/aquarius/famous", app.general.AquariusFamousHandler),
+    (r"/pisces/famous", app.general.PiscesFamousHandler),
+    (r"/aries/famous", app.general.AriesFamousHandler),
+    (r"/taurus/famous", app.general.TaurusFamousHandler),
+    (r"/gemini/famous", app.general.GeminiFamousHandler),
+    (r"/cancer/famous", app.general.CancerFamousHandler),
+    (r"/leo/famous", app.general.LeoFamousHandler),
+    (r"/virgo/famous", app.general.VirgoFamousHandler),
+    (r"/libra/famous", app.general.LibraFamousHandler),
+    (r"/scorpio/famous", app.general.ScorpioFamousHandler),
+    (r"/sagittarius/famous", app.general.SagittariusFamousHandler),
+]
 
-            (r"/capricorn/famous", app.general.CapricornFamousHandler),
-            (r"/aquarius/famous", app.general.AquariusFamousHandler),
-            (r"/pisces/famous", app.general.PiscesFamousHandler),
-            (r"/aries/famous", app.general.AriesFamousHandler),
-            (r"/taurus/famous", app.general.TaurusFamousHandler),
-            (r"/gemini/famous", app.general.GeminiFamousHandler),
-            (r"/cancer/famous", app.general.CancerFamousHandler),
-            (r"/leo/famous", app.general.LeoFamousHandler),
-            (r"/virgo/famous", app.general.VirgoFamousHandler),
-            (r"/libra/famous", app.general.LibraFamousHandler),
-            (r"/scorpio/famous", app.general.ScorpioFamousHandler),
-            (r"/sagittarius/famous", app.general.SagittariusFamousHandler),
-        ]
 
-        tornado.web.Application.__init__(self, handlers, **app_settings)
+settings = dict(
+        cookie_secret="change_me",
+        static_path=os.path.join(os.path.dirname(__file__), "static"),
+        template_path=os.path.join(os.path.dirname(__file__), "templates"),
+) 
+              
 
-def main():
-    tornado.options.define("port", default=3000, help="Listen on port", type=int)
-    tornado.options.parse_command_line()
-    logging.info("starting tornado_server on 0.0.0.0:%d" % tornado.options.options.port)
-    http_server = tornado.httpserver.HTTPServer(request_callback=Application(), xheaders=True)
-    http_server.listen(tornado.options.options.port)
-    tornado.ioloop.IOLoop.instance().start()
+application = tornado.web.Application(handlers, **settings)
 
 if __name__ == "__main__":
-    main()
+    port = os.environ['PORT'] if os.environ['PORT'] else 8888
+    application.listen(port)
+    tornado.ioloop.IOLoop.instance().start()
